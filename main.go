33c28ff59fc51cf2f253eab2f92cea35839c8a0e
0907Sowiriro <sowiriro@gmail.com>
2020-06-23 18:42:41 +0900

    first commit

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	_ "net/http"
 )
 
 func main() {
 
-	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("/public"))
-	mux.Handle("/static", http.StripPrefix("/static/", files))
+	_, err := dbConn()
+	fmt.Print(err)
+	return
 
-	mux.HandleFunc("/", Index)
-	mux.HandleFunc("/err", err)
-
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("/signup", signup)
-	mux.HandleFunc("/signup_account", signUpAccount)
-	mux.HandleFunc("/authenticate", authenticate)
-
-	mux.HandleFunc("/movie/:id", Show)
-	mux.HandleFunc("/movie/create", Create)
-	mux.HandleFunc("/movie/:id/update", Update)
-	mux.HandleFunc("/movie/:id/delete", Delete)
-
-	http.ListenAndServe(":8080", mux)
+	//mux := http.NewServeMux()
+	//files := http.FileServer(http.Dir("/public"))
+	//mux.Handle("/static", http.StripPrefix("/static/", files))
+	//
+	//mux.HandleFunc("/", Index)
+	//mux.HandleFunc("/err", err)
+	//
+	//mux.HandleFunc("/login", login)
+	//mux.HandleFunc("/logout", logout)
+	//mux.HandleFunc("/signup", signup)
+	//mux.HandleFunc("/signup_account", signUpAccount)
+	//mux.HandleFunc("/authenticate", authenticate)
+	//
+	//mux.HandleFunc("/movie/:id", Show)
+	//mux.HandleFunc("/movie/create", Create)
+	//mux.HandleFunc("/movie/:id/update", Update)
+	//mux.HandleFunc("/movie/:id/delete", Delete)
+	//
+	//http.ListenAndServe(":8080", mux)
 }
--- movie.go
+++ movie.go
@@ -1,26 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "indexを表示したしん")
+func dbConn() (db *sql.DB, err error) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "root"
+	dbName := "goblog"
+	db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		return db, err
+	}
+	return db, err
 }
-
-func Show(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "movie showを表示したしん")
-}
-
-func Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "movie createを表示したしん")
-}
-
-func Update(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "movie updateを表示したしん")
-}
-
-func Delete(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "movie deleteを表示したしん")
-}
+//
+//func Index(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintf(w, "indexを表示したしん")
+//	return
+//}
+//
+//func Show(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintf(w, "movie showを表示したしん")
+//	return
+//}
+//
+//func (movie *movie)Create(w http.ResponseWriter, r *http.Request) {
+//	db := dbConn()
+//	statement := "INSERT INTO movies (id, title, description) VALUES($1, $2, $3)"
+//	stmt, err := db.Prepare(statement)
+//	if err != nil {
+//		return
+//	}
+//	defer stmt.Close()
+//	err = stmt.QueryRow(movie.title, movie.description).Scan(&movie.Id)
+//	return
+//}
+//
+//func Update(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintf(w, "movie updateを表示したしん")
+//	return
+//}
+//
+//func Delete(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintf(w, "movie deleteを表示したしん")
+//	return
+//}
